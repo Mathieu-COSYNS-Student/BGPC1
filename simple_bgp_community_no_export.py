19b0439b4bb3892cfe380a2e4552506d9fc8af65
uybera <uyarbera@gmail>
2022-12-04 18:42:25 +0000

    refactor function name

--- simple_bgp_community_no_export.py
+++ simple_bgp_community_no_export.py
@@ -11,7 +11,7 @@ class NoExport(SimpleBGPTopo):
 
         return [community_list]
 
-    def setRouteMapsForCommunity(self, community_list, as1r1, as2r1, as2r2, as3r1):
+    def set_route_maps_for_community(self, community_list, as1r1, as2r1, as2r2, as3r1):
         for route_map_name in ['as1r1-ipv4-in', 'as1r1-ipv6-in']:
             as2r1.get_config(BGP).add_set_action(
                 name=route_map_name,
