ee213f972ca2dbac1823f69ac3c297d8e44b5157
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2022-12-28 17:36:03 +0000

    Simplify no advertise

--- simple_bgp_community_no_advertise.py
+++ simple_bgp_community_no_advertise.py
@@ -5,21 +5,5 @@ from simple_bgp_network import SimpleBGPTopo
 
 class NoAdvertiseTopo(SimpleBGPTopo):
 
-    def create_community_lists(self):
-        community_list = CommunityList(
-            'NO_ADVERTISE', community='65535:65282')
-
-        return [community_list]
-
-    def set_route_maps_for_community(self, community_list, as1r1, as2r1, as2r2, as3r1):
-        for route_map_name in ['as1r1-ipv4-in', 'as1r1-ipv6-in']:
-            as2r1.get_config(BGP).add_set_action(
-                name=route_map_name,
-                direction='in',
-                peer=as1r1,
-                matching=(community_list,),
-                set_action=RouteMapSetAction(
-                    'community',
-                    'no-advertise'
-                )
-            )
+    def send_extra_communities(self) -> str:
+        return 'no-advertise'
